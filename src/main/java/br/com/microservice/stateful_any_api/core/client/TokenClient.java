b3c48d1e7506050329cbc7d2a2a83335eafa9a6f
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-11-09 06:39:10 -0300

    chore(adicionar configuracao de http api): ajuste de api

---
+++ src/main/java/br/com/microservice/stateful_any_api/core/client/TokenClient.java
@@ -0,0 +1,4 @@
+package br.com.microservice.stateful_any_api.core.client;
+
+public interface TokenClient {
+}
---
+++ src/main/java/br/com/microservice/stateful_any_api/infra/HttpConfig.java
@@ -0,0 +1,23 @@
+package br.com.microservice.stateful_any_api.infra;
+
+import br.com.microservice.stateful_any_api.core.client.TokenClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class HttpConfig {
+
+  @Value("${app.client.base-url}")
+  private String baseUrl;
+
+  @Bean
+  public TokenClient tokenClient() {
+    return HttpServiceProxyFactory.builderFor(
+            WebClientAdapter.create(WebClient.builder().baseUrl(baseUrl).build())).build()
+        .createClient(TokenClient.class);
+  }
+}
